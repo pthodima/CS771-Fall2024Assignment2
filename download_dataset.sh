e362df3e4f9bf4095d199dac8b017e8396b6dcf4
RyanUsher606 <rnusher@wisc.edu>
2024-10-21 16:27:53 -0500

    Adding Attn

--- download_dataset.sh
+++ download_dataset.sh
@@ -4,8 +4,8 @@
 cd data
 gdown 16GYHdSWS3iMYwMPv5FpeDZN2rH7PR0F2
 # make sure that you have data.tar.gz in ./data
-wget https://raw.githubusercontent.com/CSAILVision/miniplaces/master/data/train.txt
-wget https://raw.githubusercontent.com/CSAILVision/miniplaces/master/data/val.txt
+curl -O https://raw.githubusercontent.com/CSAILVision/miniplaces/master/data/train.txt
+curl -O https://raw.githubusercontent.com/CSAILVision/miniplaces/master/data/val.txt
 
 tar -xzf data.tar.gz
 cd ..
